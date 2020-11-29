2ff991604f1a0ebbceb4c6ae8f855dfda917284f
Mehedi Ehteshum <me69oshan@gmail.com>
2020-11-28 18:31:14 -0700

    disabling caching on client’s browser using helmet.noCache(); it costs performance;

--- myApp.js
+++ myApp.js
@@ -18,6 +18,8 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge: 7776000, force: true}));
 // Disable DNS Prefetching with helmet.dnsPrefetchControl(). To improve performance, most browsers prefetch DNS records for the links in a page. In that way the destination ip is already known when the user clicks on a link. This may lead to over-use of the DNS service (if you own a big website, visited by millions people…), privacy issues (one eavesdropper could infer that you are on a certain page), or page statistics alteration (some links may appear visited even if they are not). If you have high security needs you can disable DNS prefetching, at the cost of a performance penalty.
 app.use(helmet.dnsPrefetchControl());
+// Disable Client-Side Caching with helmet.noCache(). If you are releasing an update for your website, and you want the users to always download the newer version, you can (try to) disable caching on client’s browser. It can be useful in development too. Caching has performance benefits, which you will lose, so only use this option when there is a real need.
+app.use(helmet.noCache());
 
 
 
